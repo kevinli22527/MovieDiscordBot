92b09b10da959985fbb4c053b01a2fccfe5fdfbd
Megan <mkongable18@gmail.com>
2022-12-02 19:16:22 -0500

    Leetcode Daily 12/2

--- Leetcode_Daily.py
+++ Leetcode_Daily.py
@@ -1,29 +1,19 @@
-def closeStrings(self, word1: str, word2: str) -> bool:
-    # determine if length is the same
-    # determine if the frequency of the letters are the same
-    # determine if all of one type of letter can be switched to another
-    # if all of these are true, return true
-    # if any of these are false, return false
-    # if the length is not the same, return false
-    if len(word1) != len(word2):
-        return False
-    # make a dictionary for each word
-    letterToFreq1 = {}
-    for letter in word1:
-        if letter in letterToFreq1:
-            letterToFreq1[letter] += 1
+def frequencySort(s: str) -> str:
+    # Create a dictionary to store the frequency of each character
+    freq = {}
+    for char in s:
+        if char in freq:
+            freq[char] += 1
         else:
-            letterToFreq1[letter] = 1
-    letterToFreq2 = {}
-    for letter in word2:
-        if letter in letterToFreq2:
-            letterToFreq2[letter] += 1
-        else:
-            letterToFreq2[letter] = 1
-    # if the lengths are different, the letters show in different frequencies        
-    if(len(letterToFreq1) != len(letterToFreq2)):
-        return False
-    # check if set of frequenices is the same
-    if set(letterToFreq1.values()) != set(letterToFreq2.values()):
-        return False
-    return True
+            freq[char] = 1
+    # Sort the dictionary based on the frequency of each character
+    sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+    # Create a string to store the result
+    result = ""
+    # Loop through the sorted dictionary and append the characters to the result string
+    for char, freq in sorted_freq:
+        result += char * freq
+    return result
+
+s = "ddddddaac"
+print(frequencySort(s))
