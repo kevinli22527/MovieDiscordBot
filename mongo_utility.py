0d7e39bd3547f3424395ae4b288ca29519dcfd6e
Kevin Li <54922253+kevinli22527@users.noreply.github.com>
2022-12-03 12:30:49 -0600

    added the reinitialize database functionality

--- mongo_utility.py
+++ mongo_utility.py
@@ -5,7 +5,6 @@ def getMongoClient():
 
 
 # this method drops all collections within a specified database
-# NUCLEAR: DO NOT USE UNLESS YOU KNOW WHAT YOU ARE DOING
 def drop_all_collections(target_database):
     client = getMongoClient()
     db = client[target_database]
@@ -16,6 +15,10 @@ def drop_all_collections(target_database):
 # initializes all users in the database manually. Currently, this is for putting Megan and Kevin information into the Users collection
 def initialize_users():
     client = getMongoClient()
+
+    drop_all_collections("MovieBot")  # wipe all data in the database
+
+    # reinitialize users collection
     db = client["MovieBot"]
     users = db["Users"]
 
@@ -24,6 +27,23 @@ def initialize_users():
     kevin_info = {"discord_name": "Kevin", "discord_id": "279423302408339456", "display_name": "Kevin", "watch_list": [], "turns_yielded": 0}
 
     users.insert_many([megan_info, kevin_info])
+
+    # reinitialize watched movies collection
+    watched_movies = db["WatchedMovies"]
+    watched_movies_document = {"watched_list": []}
+    watched_movies.insert_one(watched_movies_document) # this inserts the document into the collection
+
+    # reinitialize logistics collection containing the index of the current turn user along with an array of user ids that are being selected from
+    # first we get a list of all the user ids to add to the round robin selection pool
+    user_info = get_all_users()
+    user_ids = []
+    for user in user_info:
+        user_ids.append(user["discord_id"])
+
+    # set up and add logistics document
+    logistics = db["Logistics"]
+    logistics_document = {"whose_turn": 0, "users": user_ids}
+    logistics.insert_one(logistics_document) # add the document to the collection
     return
 
 
