27a371ec047acd90e7ce97fefd5048a2f0ff24bd
Kevin Li <54922253+kevinli22527@users.noreply.github.com>
2022-12-04 14:46:20 -0600

    removed old useless code

--- bot.py
+++ bot.py
@@ -41,9 +41,6 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
     await bot.process_commands(message)
 
 # test command to respond to asterisk commands
